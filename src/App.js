2cd8c2a84ca8fbae79ebf8278e1d367137f1f990
Thatsaphon Sukanantatham <thatsaphon@profilegroup-hrs.com>
2023-01-26 22:04:54 +0700

    v.2 3D

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [width, setWidth] = useState(100);
   const [height, setHeight] = useState(100);
   const [thick, setThick] = useState(3);
+  const [deep, setDeep] = useState(30);
   let box = useRef(null);
   let element;
 
@@ -28,16 +29,79 @@ function App() {
             border: "solid brown " + thick * 2 + "px",
             display: "flex",
             alignItems: "center",
+            position: "relative",
+            marginTop: `${(2 * deep) / Math.tan(45)}px`,
           }}
         >
           <div
             style={{
               borderBottom: "solid white " + thick * 2 + "px",
               width: width * 2 - 4 * thick,
+              position: "relative",
             }}
           >
             {width - 2 * thick}
+            <div
+              className="rotate"
+              style={{
+                borderBottom: `${2 * thick}px solid white`,
+                bottom: 2 * -thick,
+                left: 0,
+                width: deep * 2,
+                position: "absolute",
+              }}
+            >
+              {deep - thick}
+            </div>
           </div>
+          <div
+            className="rotate"
+            style={{
+              borderBottom: `${2 * thick}px solid indianred`,
+              top: 2 * -thick,
+              left: 0,
+              width: deep * 2,
+              position: "absolute",
+            }}
+          ></div>
+          <div
+            className="rotate"
+            style={{
+              borderBottom: `${2 * thick}px solid indianred`,
+              top: 2 * -thick,
+              right: deep * -2,
+              width: deep * 2,
+              position: "absolute",
+            }}
+          ></div>
+          <div
+            className="rotate"
+            style={{
+              borderBottom: `${2 * thick}px solid indianred`,
+              bottom: 2 * -thick,
+              right: deep * -2,
+              width: deep * 2,
+              position: "absolute",
+            }}
+          ></div>
+          <div
+            style={{
+              borderBottom: `${2 * thick}px solid indianred`,
+              top: `${(2 * deep) / -Math.tan(45) + 3 * -thick}px`,
+              left: `${(2 * deep) / Math.tan(45) + -thick}px`,
+              width: `${width * 2 - thick * 2}px`,
+              position: "absolute",
+            }}
+          ></div>
+          <div
+            style={{
+              borderRight: `${2 * thick}px solid indianred`,
+              top: `${(2 * deep) / -Math.tan(45) + 2 * -thick}px`,
+              right: `${(2 * deep) / -Math.tan(45) + 2 * -thick}px`,
+              height: `${height * 2 - thick * 2}px`,
+              position: "absolute",
+            }}
+          ></div>
         </div>
 
         <div style={{ display: "flex", flexDirection: "column" }}>
@@ -57,6 +121,15 @@ function App() {
               id="height-input"
               onChange={(e) => setHeight(e.target.value)}
               value={height}
+            />
+          </div>
+          <div>
+            <label for="deep-input">ลึก: </label>
+            <input
+              type="number"
+              id="deep-input"
+              onChange={(e) => setDeep(e.target.value)}
+              value={deep}
             />
           </div>
           <div>
